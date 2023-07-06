22643fdd00267b2ce9e99273f7addbd20c4929f4
cesarbenitez1 <cesar.benitezv92@gmail.com>
2023-07-06 13:28:11 -0500

    realicé la primera parte del proyecto integrador

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("Bienvenido!, por favor ingresa tu nombre por teclado")
+
+# solictar el nombre del jugador por teclado
+nombre_jugador = input("Por favor, ingrese su nombre: ")
+
+#Imprimir el nombre del jugador ingresado con mensaje de bievendida
+print("bienvenido" + " " + nombre_jugador  + ", " + "es hora de jugar!")
